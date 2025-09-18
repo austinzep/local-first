bb09fa3540357a4666f1bc91bac87676f0bf2870
Austin <austinzepeda1@gmail.com>
2025-09-17 19:34:00 -0500

    updated notes

--- script.R
+++ script.R
@@ -7,3 +7,18 @@ create_github_token()
 gitcreds::gitcreds_set()
 
 use_github()
+
+git_default_branch()
+git_default_branch_rename() # renames to "main" if "master" on GitHub, but does 
+# not change local repo branch name.
+git_default_branch_rediscover() # syncs R Studio with GitHub
+
+# git_default_branch_configure()
+
+# Create a new branch - click the shapes next to branch name.
+
+# production environment - finalized changes, user-ready environment.
+
+# development environment - environment for working on new features, changes.
+
+# Pull Request abbr. to PR in industry.
